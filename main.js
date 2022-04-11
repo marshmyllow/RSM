1b1807cef609557232ac6777ae8959ed8764fe96
marshmyllow <59705570+marshmyllow@users.noreply.github.com>
2022-04-11 22:29:12 +0300

    доделка скрипта

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 (function(){
 	const burderItem = document.querySelector('.burger');
 	const menu = document.querySelector('.header_container');
-	const menuCloseItem = document.querySelector('.header_container-close')
+	const menuCloseItem = document.querySelector('.header_container-close');
+	const menuLinks = document.querySelectorAll('.header_item')
 	burderItem.addEventListener('click', () => {
 		menu.classList.add('header_container_active');
 	});
 	menuCloseItem.addEventListener('click', () =>{
 		menu.classList.remove('header_container_active');
 	});
+	if (window.innerWidth <= 850) {
+		for (let i = 0; i < menuLinks.length; i += 1) {
+			menuLinks[i].addEventListener('click', () => {
+				menu.classList.remove('header_container_active');
+			});
+		}
+	}
 }());
