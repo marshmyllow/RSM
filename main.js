0fec6d0d9179b234cb7eb459827a6a8b2513a9e2
marshmyllow <59705570+marshmyllow@users.noreply.github.com>
2022-04-20 23:50:30 +0300

    Создал переключение карты при помощи js

--- main.js
+++ main.js
@@ -20,3 +20,31 @@
 
 
 
+		let msk_link = document.querySelector('.msk_link');
+        let nn_link  = document.querySelector('.nn_link');
+        let vlg_link  = document.querySelector('.vlg_link');
+        let msk_map = document.querySelector('.msk_map');
+        let nn_map = document.querySelector('.nn_map');
+        let vlg_map = document.querySelector('.vlg_map');
+
+
+
+
+        nn_link.addEventListener('click', ()=> {
+				msk_map.classList.remove('view')
+                nn_map.classList.add('view')
+                vlg_map.classList.remove('view')
+
+        })
+        
+        vlg_link.addEventListener('click', ()=> {
+            vlg_map.classList.add('view')
+            nn_map.classList.remove('view')
+            msk_map.classList.remove('view')
+        })
+
+        msk_link.addEventListener('click', ()=> {
+				vlg_map.classList.remove('view')
+                nn_map.classList.remove('view')
+                msk_map.classList.add('view')
+        })
